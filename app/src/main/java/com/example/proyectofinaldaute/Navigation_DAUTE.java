1837708e022219b6e3bbd39fabde59d350c8e65a
Marllo24 <clndres11@gmail.com>
2021-10-31 00:07:48 -0600

    nav_header
    fondo en nav_header

--- app/src/main/java/com/example/proyectofinaldaute/Navigation_DAUTE.java
+++ app/src/main/java/com/example/proyectofinaldaute/Navigation_DAUTE.java
@@ -3,30 +3,27 @@ package com.example.proyectofinaldaute;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
-
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.proyectofinaldaute.databinding.ActivityNavigationDauteBinding;
 
 public class Navigation_DAUTE extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityNavigationDauteBinding binding;
+private ActivityNavigationDauteBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityNavigationDauteBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+     binding = ActivityNavigationDauteBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarNavigationDaute.toolbar);
         binding.appBarNavigationDaute.fab.setOnClickListener(new View.OnClickListener() {
