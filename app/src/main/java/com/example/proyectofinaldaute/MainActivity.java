c85aacff464288f747d059a176fcf4c23a8b7e45
abellhd026 <alhdez052@gmail.com>
2021-11-03 16:13:36 +0000

    Correcciones

--- app/src/main/java/com/example/proyectofinaldaute/MainActivity.java
+++ app/src/main/java/com/example/proyectofinaldaute/MainActivity.java
@@ -15,41 +15,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    public EditText emailInput, passInput;
-    private Button login, gotoRegister;
-    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        emailInput = findViewById(R.id.emailInput);
-        passInput = findViewById(R.id.passInput);
 
-        login = findViewById(R.id.btnLogin);
-        gotoRegister = findViewById(R.id.btnGotoRegister);
-
-        firebaseAuth = FirebaseAuth.getInstance();
     }
-
-    public void validateEmailAddres(EditText emailInput){
-        String email;
-    }
-
-    public void login(){
-        String email = emailInput.getText().toString().trim();
-        String password = passInput.getText().toString();
-
-        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(MainActivity.this, "Has iniciado sesion correctamente!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
 
 }
 
