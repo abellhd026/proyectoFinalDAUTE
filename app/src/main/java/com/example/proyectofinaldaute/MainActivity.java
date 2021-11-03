36e2eeb8abbb55eb8ac7dc6ffa5893e8792b7dd7
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-03 09:59:15 -0600

    creacion del metodo login

--- app/src/main/java/com/example/proyectofinaldaute/MainActivity.java
+++ app/src/main/java/com/example/proyectofinaldaute/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.proyectofinaldaute;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +30,24 @@ public class MainActivity extends AppCompatActivity {
         gotoRegister = findViewById(R.id.btnGotoRegister);
 
         firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void validateEmailAddres(EditText emailInput){
+        String email;
+    }
+
+    public void login(){
+        String email = emailInput.getText().toString().trim();
+        String password = passInput.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Has iniciado sesion correctamente!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 
