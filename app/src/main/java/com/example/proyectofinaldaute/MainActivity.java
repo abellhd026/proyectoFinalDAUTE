d2dc7383355fb32343447c38d713f12441586040
abellhd026 <alhdez052@gmail.com>
2021-11-03 15:33:01 +0000

    Implementacion de la clase de Firebase

--- app/src/main/java/com/example/proyectofinaldaute/MainActivity.java
+++ app/src/main/java/com/example/proyectofinaldaute/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.proyectofinaldaute;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    public EditText emailInput, passInput;
+    private Button login, gotoRegister;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailInput = findViewById(R.id.emailInput);
+        passInput = findViewById(R.id.passInput);
+
+        login = findViewById(R.id.btnLogin);
+        gotoRegister = findViewById(R.id.btnGotoRegister);
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
+
+
 }
+
