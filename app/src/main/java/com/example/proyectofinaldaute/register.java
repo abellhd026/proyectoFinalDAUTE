5d67c56982c5c363b31194c152ed2003ecaacd6b
CristianAlvarez2003 <cris0192776@gmail.com>
2021-11-04 09:53:30 -0600

    Actividad Guardar Registros

--- app/src/main/java/com/example/proyectofinaldaute/register.java
+++ app/src/main/java/com/example/proyectofinaldaute/register.java
@@ -1,23 +1,47 @@
 package com.example.proyectofinaldaute;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class register extends AppCompatActivity {
+
+    EditText emailInput, passInput;
+    FirebaseAuth firebaseAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
     public void register() {
-
-        if (validateEmailAdress(emailImput))  {
-
-            String email = emailImput.getText().toString().trim();
-        }
+        
+        String email = emailInput.getText().toString().trim();
+        String password = passInput.getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(register.this, "EL REGISTROS FUE CORRECTO", Toast.LENGTH_SHORT).show();
+                } else {
+                    String e = task.getException().toString();
+                    Toast.makeText(register.this, e, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-
 }
