951c21b9a9f9bcb525698a37f69ea441349d0e27
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-04 12:01:00 -0600

    Se agrego el fragmento actualizar perfil

---
+++ app/src/main/java/com/example/proyectofinaldaute/ui/Actualizar_Perfil/Actualizar_Perfil.java
@@ -0,0 +1,22 @@
+package com.example.proyectofinaldaute.ui.Actualizar_Perfil;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.proyectofinaldaute.R;
+
+public class Actualizar_Perfil extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_actualizar__perfil, container, false);
+    }
+}
