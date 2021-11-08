aa411e9612ae325bef768f2729318d755ef971fe
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-08 13:08:45 -0600

    se agrego el metodo saveServer en la clase categoria, ademas de eso se agrego la clase MySingleton ya que no se habia agregado al proyecto.

--- app/src/main/java/com/example/proyectofinaldaute/ui/Categorias/categorias.java
+++ app/src/main/java/com/example/proyectofinaldaute/ui/Categorias/categorias.java
@@ -1,5 +1,7 @@
 package com.example.proyectofinaldaute.ui.Categorias;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.proyectofinaldaute.MySingleton;
 import com.example.proyectofinaldaute.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class categorias extends Fragment {
@@ -20,4 +35,81 @@ public class categorias extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_categorias, container, false);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void saveServer (final Context context, final int id_cat, final String name_cat, final int est_cat){
+        String url = "https://defunctive-loran.000webhostapp.com/guardarCategotia.php";
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
+
+            @Override
+            public void onResponse(String response) {
+                JSONObject requestJSON;
+                try {
+                    requestJSON = new JSONObject(response.toString());
+                    String estado = requestJSON.getString("estado");
+                    String mensaje = requestJSON.getString("mensaje");
+
+                    if(estado.equals("1")){
+                        Toast.makeText(context, mensaje, Toast.LENGTH_SHORT).show();
+
+                    }else if(estado.equals("2")){
+                        Toast.makeText(context, mensaje, Toast.LENGTH_SHORT).show();
+                    }
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Error en la conexion" + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String, String>  map = new HashMap<>();
+                map.put("Content-Type", "application/json; charset=utf-8");
+                map.put("Accept", "application/json");
+                map.put("id", String.valueOf(id_cat));
+                map.put("nombre", name_cat);
+                map.put("estado", String.valueOf(est_cat));
+                return map;
+
+            }
+
+        };
+        MySingleton.getInstance(context).addToRequestQueue(request);
+    }//Fin del metodo saveServer
+
+
+
 }
