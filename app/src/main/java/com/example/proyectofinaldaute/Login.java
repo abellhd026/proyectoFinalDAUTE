28c4feb1e329024ea8582750e404b83ac2c9c9c6
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-09 21:11:44 -0600

    Se elimino el ActionBar del login.

--- app/src/main/java/com/example/proyectofinaldaute/Login.java
+++ app/src/main/java/com/example/proyectofinaldaute/Login.java
@@ -28,7 +28,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        getSupportActionBar().hide();
         emailInput = findViewById(R.id.emailInput);
         passInput = findViewById(R.id.passInput);
 
