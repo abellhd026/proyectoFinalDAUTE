8e3e53ea97228d248e6809ca0ff438e91a65ca69
CRISTIAN_ALVAREZ <cris0192776@gmail.com>
2021-11-11 10:53:28 -0600

    metodo modificarProductos

--- app/src/main/java/com/example/proyectofinaldaute/modificarProductos.java
+++ app/src/main/java/com/example/proyectofinaldaute/modificarProductos.java
@@ -2,13 +2,96 @@ package com.example.proyectofinaldaute;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class modificarProductos extends AppCompatActivity {
+
+    ListView listadoProductos;
+
+    ArrayList<String> lista = null;
+    ArrayList<dto_productos> listaProductos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar_productos);
+    }
+    public void mostrarProductos(final Context context) {
+
+        listaProductos = new ArrayList<dto_productos>();
+        lista = new ArrayList<String>();
+        String url = "https://defunctive-loran.000webhostapp.com/getAllProductos.php";
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                ArrayList<String> values = new ArrayList<String>();
+
+                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+
+                try {
+
+                    JSONArray array = new JSONArray(response);
+
+                    int totalEncontrados = array.length();
+
+                    dto_productos objProductos = null;
+
+                    for (int i = 0; i < array.length(); i++) {
+
+                        JSONObject productosObject = array.getJSONObject(i);
+                        int id_categoria = Integer.parseInt(productosObject.getString("id"));
+                        String nombre_categoria = productosObject.getString("nombre");
+                        double precio = Double.parseDouble(productosObject.getString("precio"));
+
+
+                        objProductos = new dto_productos(id_categoria, nombre_categoria, precio);
+
+                        listaProductos.add(objProductos);
+
+                        lista.add(listaProductos.get(i).getId() + "-" + listaProductos.get(i).getNombre());
+
+
+
+
+                        values.add(String.valueOf(id_categoria) + " - " + nombre_categoria );
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1, values);
+                        listadoProductos.setAdapter(adapter);
+                    }
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "ERROR EN LA CONEXION DE INTERNET", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MySingleton.getInstance(context).addToRequestQueue(request);
     }
 }
