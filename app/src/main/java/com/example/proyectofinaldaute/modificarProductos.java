83809e60eb6608572090226b57e9e144dfcdc492
Marllo 24 <clndres11@gmail.com>
2021-11-11 09:55:45 -0600

    actividad_modificar y eliminar productos

---
+++ app/src/main/java/com/example/proyectofinaldaute/modificarProductos.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinaldaute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modificarProductos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_productos);
+    }
+}
