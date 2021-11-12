b2135d596b6cc61e076b9bb66d9926b7b79e301e
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-12 00:18:57 -0600

    Se Agrego Un Splash al Inicio de la app, para que muestre el progreso, se configuro tambien la clase java Splash para su duracion.

---
+++ app/src/main/java/com/example/proyectofinaldaute/Splash.java
@@ -0,0 +1,30 @@
+package com.example.proyectofinaldaute;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 6000);
+
+    }
+}
