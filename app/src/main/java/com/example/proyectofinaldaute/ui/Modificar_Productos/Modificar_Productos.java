96f93d5290475d11ba748065127b86998dc00359
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-14 22:55:31 -0600

    Se creo un package, Modificar_Productos

--- app/src/main/java/com/example/proyectofinaldaute/ui/Modificar_Productos/Modificar_Productos.java
+++ app/src/main/java/com/example/proyectofinaldaute/ui/Modificar_Productos/Modificar_Productos.java
@@ -1,4 +1,4 @@
-package com.example.proyectofinaldaute;
+package com.example.proyectofinaldaute.ui.Modificar_Productos;
 
 import android.os.Bundle;
 
@@ -8,9 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.proyectofinaldaute.R;
+
 
 public class Modificar_Productos extends Fragment {
-    
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
